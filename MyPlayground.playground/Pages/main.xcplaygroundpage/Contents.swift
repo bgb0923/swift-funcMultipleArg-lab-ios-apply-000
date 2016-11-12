0dabe3304c9ba761aa7c9df2cf1c90b62e7b1efe
William Brancato <bgb0923@Mac-mini.local>
2016-11-12 18:46:30 -0500

    Responded to Questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,13 +15,14 @@
  */
 // write your code here
 
-
-
+func quantityOfCountries(continent: String, numberOfCountries: Int){
+    print("\(continent) is a continent which contains \(numberOfCountries) countries")
+}
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greeting(name: String, _ greeting: String) {
     print("\(greeting), \(name)!")
 }
 
@@ -30,10 +31,10 @@ greeting(name: "Danny", "Hello")
 
 
 
-/*: question3
- ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
- */
-func daysInMonth(month: String, days: String) {
+/////*: question3
+//// ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
+//// */
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -42,14 +43,18 @@ daysInMonth(month: "November", days: 30)
 
 
 
-/*: question4
- ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
- */
+/////*: question4
+//// ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
+//// */
 // write your code here
 
 
+func beverage(numberOfBottles: Int, beverage: String, wallType: String){
+    print("\(numberOfBottles) bottles of \(beverage) on the \(wallType) wall")
+}
 
+beverage(numberOfBottles: 99, beverage: "beer", wallType: "brick")
 
-/*:
- Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
- */
+/////*:
+// Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
+// */
